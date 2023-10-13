5ff4a0af26e41ecfab760b70ba8114b831bc933c
Marcos Allan <106402058+Marcos-Allan@users.noreply.github.com>
2023-10-13 17:30:17 -0300

    Deixei a página table funcional

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let elements = [...document.querySelectorAll('.element')]
 let sElem = []
+let gt = 0.9
+let atual = 0
 
+// FETCH E ORGANIZAÇÃO DO CONTEUDO NA TABELA
 let tablePeriodic = './js/tp.json'
 fetch(tablePeriodic, {
     method: 'GET',
@@ -8,7 +11,6 @@ fetch(tablePeriodic, {
 })
 .then((response) => response.json())
 .then((data) => {
-    // console.log(data)
     for(let i = 0; i < elements.length; i++){
         sElem.push(data[i].symbol)
         elements[i].innerText = data[i].symbol   
@@ -30,9 +32,11 @@ fetch(tablePeriodic, {
 
 .catch((err) => console.log(err))
 
+// FUNÇÃO QUE MOSTRA O ELEMENTO ESCOLHIDO
 function mostrar(elem, name){
     const chosenElement = document.querySelector('#chosenElement')
-    colored()
+    colored(elements)
+    const colorElem = elem.style.backgroundColor
     elem.style.backgroundColor='white'
     
     fetch(tablePeriodic, {
@@ -41,19 +45,29 @@ function mostrar(elem, name){
     })
     .then((response) => response.json())
     .then((data) => {
-        chosenElement.innerHTML = ''
-        data.map((elem) => {
+        if(chosenElement.children[2]){
+            chosenElement.children[2].remove()
+        }
+
+        data.map((elem, i) => {
             if(elem.symbol == name){
-                let img1 = document.createElement('img')
-                img1.src = '../imgs/esquerda.png'
-                let img2 = document.createElement('img')
-                img2.src = '../imgs/direita.png'
+                let res = document.createElement('div')
+                res.classList.add('divRes')
                 let divEl = document.createElement('div')
+                let dsitributionEletronic = document.createElement('p')
+                dsitributionEletronic.classList.add('dsitribuitionElectric')
+                let elemName = document.createElement('p')
+                elemName.classList.add('nameNumber')
+                dsitributionEletronic.innerHTML = elem.electronicConfiguration
+                elemName.innerHTML = `<span>#${elem.atomicNumber}</span> ${elem.name}`
                 divEl.innerText = elem.symbol
+                divEl.style.backgroundColor = colorElem
+                divEl.style.backgroundColor = 1
                 divEl.classList.add('elemSymb')
-                chosenElement.appendChild(img1)
-                chosenElement.appendChild(divEl)
-                chosenElement.appendChild(img2)
+                res.appendChild(divEl)
+                res.appendChild(elemName)
+                res.appendChild(dsitributionEletronic)
+                chosenElement.appendChild(res)
                 return
             }
         })
@@ -61,16 +75,691 @@ function mostrar(elem, name){
     .catch((err) => console.log(err))
 }
 
-elements.map((elem) => {
+// ADICIONA EVENTO DE CLIQUE NOS ITENS DA TABELA
+elements.map((elem, i) => {
     elem.addEventListener('click', (el) => {
-        console.log(el.target.innerText)
+        atual = i
         mostrar(el.target, el.target.innerText)
     })
 })
 
+let prev = document.querySelector('.prev') 
+let prox = document.querySelector('.prox') 
 
-let gt = 0.9
-function colored(){
+function prevElem() {
+    if(atual == 0){
+        return
+    }else if(atual == 56){
+        atual = 103
+    }else if(atual == 88){
+        atual = 56
+    }else if(atual == 103) {
+        atual = 73
+    }
+    else if(atual == 73){
+        atual = 118
+    }
+    atual-=1
+    mostrar(elements[atual], sElem[atual])
+    let tt = 0
+    switch (atual) {
+        case 88:
+            tt = 56
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 89:
+            tt = 57
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 90:
+            tt = 58
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 91:
+            tt = 59
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 92:
+            tt = 60
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 93:
+            tt = 61
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 94:
+            tt = 62
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 95:
+            tt = 63
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 96:
+            tt = 64
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 97:
+            tt = 65
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 98:
+            tt = 66
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 99:
+            tt = 67
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 100:
+            tt = 68
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 101:
+            tt = 69
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 102:
+            tt = 70
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 56:
+            tt = 71
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 57:
+            tt = 72
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 58:
+            tt = 73
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 59:
+            tt = 74
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 60:
+            tt = 75
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 61:
+            tt = 76
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 62:
+            tt = 77
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 63:
+            tt = 78
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 64:
+            tt = 79
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 65:
+            tt = 80
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 66:
+            tt = 81
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 67:
+            tt = 82
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 68:
+            tt = 83
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 69:
+            tt = 84
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 70:
+            tt = 85
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 71:
+            tt = 86
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 72:
+            tt = 87
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 73:
+            tt = 103
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 74:
+            tt = 104
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 75:
+            tt = 105
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 76:
+            tt = 106
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 77:
+            tt = 107
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 78:
+            tt = 108
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 79:
+            tt = 109
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 80:
+            tt = 110
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 81:
+            tt = 111
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 82:
+            tt = 112
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 83:
+            tt = 113
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 84:
+            tt = 114
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 85:
+            tt = 115
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 86:
+            tt = 116
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 87:
+            tt = 117
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 103:
+            tt = 88
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 104:
+            tt = 89
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 105:
+            tt = 90
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 106:
+            tt = 91
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 107:
+            tt = 92
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 108:
+            tt = 93
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 109:
+            tt = 94
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 110:
+            tt = 95
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 111:
+            tt = 96
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 112:
+            tt = 97
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 113:
+            tt = 98
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 114:
+            tt = 99
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 115:
+            tt = 100
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 116:
+            tt = 101
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 117:
+            tt = 102
+            mostrar(elements[atual], sElem[tt])
+        break;
+    }
+}
+
+function nextElem(){
+    if(atual == 87){
+        atual = -1
+    }else if(atual == 55){
+        atual = 87
+    }else if (atual == 102) {
+        atual = 55   
+    }
+    else if(atual == 72){
+        atual = 102
+    }
+    else if(atual == 117){
+        atual = 72
+    }
+    atual+=1
+    mostrar(elements[atual], sElem[atual])
+    let tt = 0
+    switch (atual) {
+        case 88:
+            tt = 56
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 89:
+            tt = 57
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 90:
+            tt = 58
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 91:
+            tt = 59
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 92:
+            tt = 60
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 93:
+            tt = 61
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 94:
+            tt = 62
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 95:
+            tt = 63
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 96:
+            tt = 64
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 97:
+            tt = 65
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 98:
+            tt = 66
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 99:
+            tt = 67
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 100:
+            tt = 68
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 101:
+            tt = 69
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 102:
+            tt = 70
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 56:
+            tt = 71
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 57:
+            tt = 72
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 58:
+            tt = 73
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 59:
+            tt = 74
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 60:
+            tt = 75
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 61:
+            tt = 76
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 62:
+            tt = 77
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 63:
+            tt = 78
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 64:
+            tt = 79
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 65:
+            tt = 80
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 66:
+            tt = 81
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 67:
+            tt = 82
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 68:
+            tt = 83
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 69:
+            tt = 84
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 70:
+            tt = 85
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 71:
+            tt = 86
+            mostrar(elements[atual], sElem[tt])
+        break;
+        
+        case 72:
+            tt = 87
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 73:
+            tt = 103
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 74:
+            tt = 104
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 75:
+            tt = 105
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 76:
+            tt = 106
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 77:
+            tt = 107
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 78:
+            tt = 108
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 79:
+            tt = 109
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 80:
+            tt = 110
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 81:
+            tt = 111
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 82:
+            tt = 112
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 83:
+            tt = 113
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 84:
+            tt = 114
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 85:
+            tt = 115
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 86:
+            tt = 116
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 87:
+            tt = 117
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 103:
+            tt = 88
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 104:
+            tt = 89
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 105:
+            tt = 90
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 106:
+            tt = 91
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 107:
+            tt = 92
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 108:
+            tt = 93
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 109:
+            tt = 94
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 110:
+            tt = 95
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 111:
+            tt = 96
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 112:
+            tt = 97
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 113:
+            tt = 98
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 114:
+            tt = 99
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 115:
+            tt = 100
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 116:
+            tt = 101
+            mostrar(elements[atual], sElem[tt])
+        break;
+
+        case 117:
+            tt = 102
+            mostrar(elements[atual], sElem[tt])
+        break;
+    }
+}
+
+prev.addEventListener('click', prevElem)
+prox.addEventListener('click', nextElem)
+
+window.addEventListener('keyup', (evt) => {
+    if(evt.key == 'ArrowLeft'){
+        prevElem()
+    }
+    
+    if(evt.key == 'ArrowRight'){
+        nextElem()
+    }
+})
+
+//FUNÇÃO PARA COLORIR ITENS DA TABELA DE ACORDO COM O GRUPO
+function colored(elements){
     elements.map((el, i) => {
 
         switch (i) {
